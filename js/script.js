b3191d933303ed954b38ea1a58f4bc53b94e81fb
doraaan89 <doraaan89@gmail.com>
2022-05-14 23:56:24 +0900

    header 작업중

---
+++ js/script.js
@@ -0,0 +1,117 @@
+const gnb = new Vue({
+  el: '#gnb',
+  data: {
+    gnbArr: [
+      {
+        title: '111',
+        url: '',
+        depth2: [
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+        ]
+      },
+      {
+        title: '111',
+        url: '',
+        depth2: [
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+        ]
+      },
+      {
+        title: '111',
+        url: '',
+        depth2: [
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+        ]
+      },
+      {
+        title: '111',
+        url: '',
+        depth2: [
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+        ]
+      },
+      {
+        title: '111',
+        url: '',
+        depth2: [
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+          {
+            title: '1112',
+            url: '',
+          },
+        ]
+      },
+    ]
+  }
+})
