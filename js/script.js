efbddc45f058195d0db46608c37cd812c8d59eff
doraaan89 <doraaan89@gmail.com>
2022-06-25 18:25:41 +0900

    trainer page 수정중

--- js/script.js
+++ js/script.js
@@ -168,17 +168,133 @@ const trainer = new Vue({
 			trainerArr: [
 				{
 					id: 'vocal',
+          title: 'VOCAL',
 					ceo: true,
 					trainer: [
 						{
-							name: 'ttt',
-							profileImg: '',
-						}
+							profileImg: 'temp/profile/강사 사진/김찬오T_2.jpg',
+							name: '김 찬 오',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/정앙정T.bmp',
+							name: '정 앙 정',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/최민정T.jpeg',
+							name: '최 민 정',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/이하정T.jpg',
+							name: '이 하 정',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/전승원.jpg',
+							name: '전 승 원',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/고유진1.jpg',
+							name: '고 리 하',
+						},
+					]
+				},
+				{
+					id: 'dance',
+          title: 'DANCE',
+					trainer: [
+						{
+							profileImg: 'temp/profile/강사 사진/영진 프로필.jpg',
+							name: '김 영 진',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/상민샘1.jpg',
+							name: '김 상 민',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/고유진1.jpg',
+							name: '고 리 하',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/이하정T.jpg',
+							name: '이 하 정',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/소서현T.jpg',
+							name: '소 서 현',
+						},
+					]
+				},
+				{
+					id: 'midi',
+          title: 'MIDI',
+					trainer: [
+						{
+							profileImg: 'temp/profile/강사 사진/매트루스T.jpeg',
+							name: '매트루스',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/김준형.jpg',
+							name: '김 준 형',
+						},
+					]
+				},
+				{
+					id: 'rap',
+          title: 'RAP',
+					trainer: [
+						{
+							profileImg: 'temp/profile/강사 사진/영진 프로필.jpg',
+							name: '김 영 진',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/상민샘1.jpg',
+							name: '김 상 민',
+						},
+					]
+				},
+				{
+					id: 'piano',
+          title: 'PIANO',
+					trainer: [
+						{
+							profileImg: 'temp/profile/강사 사진/안소현T.jpg',
+							name: '안 소 현',
+						},
+					]
+				},
+				{
+					id: 'guitar',
+          title: 'GUITAR',
+					trainer: [
+						{
+							profileImg: 'temp/profile/강사 사진/민우t.jpg',
+							name: '지 민 우',
+						},
+						{
+							profileImg: 'temp/profile/강사 사진/김준형.jpg',
+							name: '김 준 형',
+						},
 					]
 				},
 			],
+      modalName: '',
+      isModal: false,
 		}
-	}
+	},
+  methods: {
+    modalOpen(txt) {
+      this.modalName = txt.split(' ').join('');
+      this.isModal = true;
+    },
+    modalClose() {
+      this.isModal = false;
+    },
+    prevBtn() {
+
+    },
+    nextBtn() {
+
+    },
+  }
 });
 
 const facility = new Vue({
